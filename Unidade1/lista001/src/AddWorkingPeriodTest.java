674928e2422aab06b00e38061d20c2b54ebf005b
Débora Costa <debaemili@gmail.comm>
2018-04-08 22:45:58 -0300

    adicionado mais testes

--- Unidade1/lista001/src/AddWorkingPeriodTest.java
+++ Unidade1/lista001/src/AddWorkingPeriodTest.java
@@ -4,22 +4,68 @@ import org.junit.jupiter.api.Test;
 class AddWorkingPeriodTest {
 	
 	@Test
-	void addWorkingPeriodTest_correto() {
+	void addWorkingPeriodTest_correto3() {
 		int tempo_cronograma = 5;
 		WorkSchedule teste = new WorkSchedule(tempo_cronograma);		
-		teste.setRequiredNumber(2, 0, 1);
+		teste.setRequiredNumber(2, 0, 2);
 		String empregado = "Deba";
-		boolean res = teste.addWorkingPeriod(empregado, 0, 1);
+		boolean res = teste.addWorkingPeriod(empregado, 0, 2);
 		Assert.assertEquals(true, res);
+		
 	}
 	
 	@Test
-	void addWorkingPeriodTest_entradasErradas() {
+	void addWorkingPeriodTest_correto1() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);		
+		teste.setRequiredNumber(2, 0, 0);
+		String empregado = "Deba";
+		boolean res = teste.addWorkingPeriod(empregado, 0, 0);
+		Assert.assertEquals(true, res);
+		
+	}
+	@Test
+	void addWorkingPeriodTest_correto2() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);		
+		teste.setRequiredNumber(2, 4, 4);
+		String empregado = "Deba";
+		boolean res = teste.addWorkingPeriod(empregado, 4, 4);
+		Assert.assertEquals(true, res);
+		
+	}
+	
+	@Test
+	void addWorkingPeriodTest_entradasErradas1() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		teste.setRequiredNumber(2, 0, 1);
+		String empregado = "Deba";
+		boolean res = teste.addWorkingPeriod(empregado, -1, 4);
+		Assert.assertEquals(false, res);	}
+	@Test
+	void addWorkingPeriodTest_entradasErradas2() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		teste.setRequiredNumber(2, 0, 1);
+		String empregado = "Deba";
+		boolean res = teste.addWorkingPeriod(empregado, 0, 6);
+		Assert.assertEquals(false, res);	}
+	@Test
+	void addWorkingPeriodTest_entradasErradas3() {
 		int tempo_cronograma = 5;
 		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
 		teste.setRequiredNumber(2, 0, 1);
 		String empregado = "Deba";
 		boolean res = teste.addWorkingPeriod(empregado, -1, 6);
+		Assert.assertEquals(false, res);	}
+	@Test
+	void addWorkingPeriodTest_entradasErradas4() {
+		int tempo_cronograma = 7;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		teste.setRequiredNumber(2, 0, 1);
+		String empregado = "Deba";
+		boolean res = teste.addWorkingPeriod(empregado, 6, 5);
 		Assert.assertEquals(false, res);	}
 	
 	@Test
@@ -41,8 +87,6 @@ class AddWorkingPeriodTest {
 		String empregado = "Deba";
 		boolean res = teste.addWorkingPeriod(empregado, 0, 1);
 		Assert.assertEquals(false, res);
-	}
-
-	
+	}	
 
 }
--- Unidade1/lista001/src/WorkingEmployeesTest.java
+++ Unidade1/lista001/src/WorkingEmployeesTest.java
@@ -1,32 +1,12 @@
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class WorkingEmployeesTest {
 	
 	@Test
-	void addWorkingPeriodTest_mesmoNome() {
-		int tempo_cronograma = 5;
-		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
-		teste.setRequiredNumber(2, 0, 1);
-		String empregado = "Deba";
-		teste.addWorkingPeriod(empregado, 0, 1);
-		String empregado1 = "Deba";
-		boolean res1 = teste.addWorkingPeriod(empregado1, 0, 1);
-		Assert.assertEquals(false, res1);
-	}
-	@SuppressWarnings("deprecation")
-	@Test
-	private void workingEmployeesTest_errado() {
-		int tempo_cronograma = 5;
-		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
-		String[] esp = {};
-		String[] res = teste.workingEmployees(3, 1);
-		Assert.assertEquals(esp, res);
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Test
-	private void workingEmployeesTest_certo() {
+	void workingEmployeesTest_certo() {
 		int tempo_cronograma = 5;
 		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
 		teste.setRequiredNumber(3, 0, 1);
@@ -35,8 +15,44 @@ class WorkingEmployeesTest {
 			teste.addWorkingPeriod(empregados[i], 0, 1);
 		}
 		String[] res = teste.workingEmployees(0, 1);
-		Assert.assertEquals(empregados, res);
+
+		Assert.assertEquals(true, Arrays.equals(empregados, res));
 	}
 	
-
+	@Test
+	void workingEmployeesTest_s_igual_e() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		String[] esp = {};
+		String[] res = teste.workingEmployees(3, 3);
+		Assert.assertEquals(true, Arrays.equals(esp, res));
+	}
+	
+	@Test
+	void workingEmployeesTest_s_maior_e() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		String[] esp = {};
+		String[] res = teste.workingEmployees(3, 1);
+		Assert.assertEquals(true, Arrays.equals(esp, res));
+	}
+	
+	
+	@Test
+	void workingEmployeesTest_s_maior_tamanho() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		String[] esp = {};
+		String[] res = teste.workingEmployees(6, 1);
+		Assert.assertEquals(false, Arrays.equals(esp, res));
+	}
+	
+	@Test
+	void workingEmployeesTest_tamanho_menor_0() {
+		int tempo_cronograma = 5;
+		WorkSchedule teste = new WorkSchedule(tempo_cronograma);
+		String[] esp = {};
+		String[] res = teste.workingEmployees(1, -1);
+		Assert.assertEquals(false, Arrays.equals(esp, res));
+	}
 }
