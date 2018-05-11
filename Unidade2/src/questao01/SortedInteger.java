9605beedad949a862335113447fd398a841b7aeb
FagnerDias <fagnerdiasmorais@gmail.com>
2018-05-11 12:49:26 -0300

    finalizado a questão 1 - a

--- Unidade2/src/questao01/SortedInteger.java
+++ Unidade2/src/questao01/SortedInteger.java
@@ -1,40 +1,93 @@
 package questao01;
 
 public class SortedInteger {
-	private int arr[];
-	private int capacity, size = 0;
+	/*@ public invariant (\forall int i; 
+	 * 						0 < i && i < size;
+	 * 						arr[i - 1] <= arr[i]);
+	 * 
+	 *  */
+	//@ public invariant 0 <= size && size <= arr.length
+	  
+	
+	private /*@ spec_public @*/ int arr[];
+	private /*@ spec_public @*/ int capacity, size = 0;
 	
 	public SortedInteger(int capacity) {
 		this.capacity = capacity;
 		this.arr = new int[capacity];
 	}
 	
+	/*@ public normal_behavior
+	 *  requires size < capacity;
+	 *  
+	 *  ensures size <= capacity;
+	 *  ensures size == \old(size) + 1;
+	 *  ensures contains(elem);
+	 *  ensures (\forall int e;
+	 *  			e != elem;
+	 *  			contains(e) <==> \old(contains(e)));
+	 *  
+	 * */
 	public void add(int elem) {
 	// ...
 	}
 	
+	/*@ public normal_behavior
+	 *  requires size > 0;
+	 *  requires capacity > 0;
+	 *  
+	 *  ensures size <= capacity;
+	 *  ensures size == \old(size) - 1;
+	 *  ensures !contains(elem);
+	 *  ensures (\forall int e;
+	 *  			e != elem;
+	 *  			contains(e) <==> \old(contains(e)));
+	 *  ensure \old(contains(elem)) 
+	 *  		==> size == \old(size) - 1;
+	 *  ensure !\old(contains(elem)) 
+	 *  		==> size == \old(size);
+	 *  
+	 * */
 	public void remove(int elem) {
 	// ...
 	}
 	
-	public boolean contains(int elem) {
+	/*@ public normal_behavior
+	 * 		
+	 * 	ensures \result == (\exist int i;
+	 * 							0 < i && i > size;
+	 * 							arr[i] == elem);
+	 * 
+	 * */
+	public /*@ pure */ boolean contains(int elem) {
 	// ...
 		return false;
 	}
 	
-	public int max() {
+	/*@ public normal_behavior
+	 * 	
+	 * 	requires size != 0
+	 * 
+	 *  ensures \result == arr[size]
+	 * 
+	 * */
+	public /*@ pure */ int max() {
 	// ...
 		return 0;
 	}
 	
-	public int getSize() {
-	// ...
-		return 0;
+	/*@ public normal_behavior
+    @ ensures \result == size;
+    @*/
+	public /*@ pure */ int getSize() {
+		return this.size;
 	}
 	
-	public int getCapacity() {
-	// ...
-		return 0;
+	/*@ public normal_behavior
+    @ ensures \result == capacity;
+    @*/
+	public /*@ pure */ int getCapacity() {
+		return this.capacity;
 	}
 	
 	public String toString() {
