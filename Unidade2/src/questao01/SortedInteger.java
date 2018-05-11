fbb95c610200243c43722982552742a5efd7ebfb
FagnerDias <fagnerdiasmorais@gmail.com>
2018-05-11 11:14:25 -0300

    iniciando trabalho da unidade 2

---
+++ Unidade2/src/questao01/SortedInteger.java
@@ -0,0 +1,45 @@
+package questao01;
+
+public class SortedInteger {
+	private int arr[];
+	private int capacity, size = 0;
+	
+	public SortedInteger(int capacity) {
+		this.capacity = capacity;
+		this.arr = new int[capacity];
+	}
+	
+	public void add(int elem) {
+	// ...
+	}
+	
+	public void remove(int elem) {
+	// ...
+	}
+	
+	public boolean contains(int elem) {
+	// ...
+		return false;
+	}
+	
+	public int max() {
+	// ...
+		return 0;
+	}
+	
+	public int getSize() {
+	// ...
+		return 0;
+	}
+	
+	public int getCapacity() {
+	// ...
+		return 0;
+	}
+	
+	public String toString() {
+	// ...
+		return null;
+	}
+
+}
